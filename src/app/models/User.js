7d34bb2a28b8dc49cde3899753ddf5e22e1dcefd
Matheus Arthus <matheusarthus@Matheuss-MacBook-Pro.local>
2021-02-25 22:28:21 -0300

    users model

---
+++ src/app/models/User.js
@@ -0,0 +1,22 @@
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        cpf: Sequelize.STRING,
+        birth_date: Sequelize.DATE,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+}
+
+export default User;
