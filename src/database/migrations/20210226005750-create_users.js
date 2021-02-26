cd1925627a20186528e10ce650638a5d61d815f7
Matheus Arthus <matheusarthus@Matheuss-MacBook-Pro.local>
2021-02-25 22:19:58 -0300

    users migration

---
+++ src/database/migrations/20210226005750-create_users.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      birth_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
