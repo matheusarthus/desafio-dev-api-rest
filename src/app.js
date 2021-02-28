593df4940cacbe1360b44cabf2093c79383f4231
Matheus Arthus <matheusarthus@Matheuss-MacBook-Pro.local>
2021-02-28 17:38:05 -0300

    cors setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const log4js = require('log4js');
+const cors = require('cors');
 const setupRoutes = require('./routes');
 
 const logger = log4js.getLogger('[App]');
@@ -8,6 +9,7 @@ const logger = log4js.getLogger('[App]');
 module.exports = function configureExpressApp(app) {
   logger.debug('Starting to configure Express App');
 
+  app.use(cors());
   app.use(bodyParser.json());
   setupRoutes(app);
 
